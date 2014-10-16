edaa24a714b9be2fefe7de692bb95acaae6fd59d
RyanLongRoad <4327@longroad.ac.uk>
2014-10-16 12:16:21 +0100

    commit

---
+++ n! development exercise.py
@@ -0,0 +1,37 @@
+#Ryan Cox
+#16/10/14
+#Development exercises
+#not working right now
+
+
+total = 0
+number = 0
+increase = 1
+
+repeats = int(input("Please enter the value of n: "))
+
+for count in range(repeats):
+    number = number + increase
+    total = number + increase
+    
+    
+
+print(total)
+    
+   
+    
+    
+
+
+    
+    
+  
+    
+    
+                
+
+
+
+   
+
+    
