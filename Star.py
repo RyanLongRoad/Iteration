289b3381500d79f7be1a2034d3fb6928282d64ea
RyanLongRoad <44327@longrong.ac.uk>
2014-10-20 21:17:18 +0100

    Stars printer
    A program that prints stars for the user

---
+++ Star.py
@@ -0,0 +1,10 @@
+#Ryan Cox
+#20/10/14
+#iteration stars
+
+
+stars = int(input("Please enter the number of stars you want per row: "))
+rows = int(input("Please enter the number of rows you want displayed: "))
+
+for count in range(rows):
+              print("*" * stars)
